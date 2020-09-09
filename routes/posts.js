13431a6cbfaec0b7ef8ded22b4e42d71a0b8caf8
agwenchez <agweraenock76@gmail.com>
2020-09-09 11:10:01 -0400

    added server-side validation

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const Post = require('../models/Posts')
 const {createPostValidator}= require('../validation/posts');
-const {createPost} = require('../controllers/posts');
 
 router.get('/',(req,res)=>{
     res.status(500).send('This route works');
 })
 
 // create new post
-router.post('/new', createPostValidator ,createPost)
+router.post('/new', createPostValidator ,(req, res)=>{
+    data = req.body
+
+   const newPost = new Post(data);
+   newPost.save()
+       .then( result =>{
+           res.status(201).json(result);
+       })
+       .catch(err => console.log(err));
+
+})
 
 module.exports = router;
