aa748a30487aa93fd7f19577db7768ae300f21ba
agwenchez <agweraenock76@gmail.com>
2020-09-10 04:08:30 -0400

    some minor bug fixes

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const uuidv1 = require('uuid/v1')
+const crypto = require('crypto');
 const userSchema = new Schema({
     name:{
         type:String,
@@ -10,7 +11,7 @@ const userSchema = new Schema({
         type:String,
         required:true
     },
-    password:{
+    password_hash:{
         type:String,
         required:true
     },
@@ -24,6 +25,36 @@ const userSchema = new Schema({
 })
 
 
+// virtual fields
+userSchema.virtual(password)
+.set( password=>{
 
+// create a temp field _password
+this._password = password
+
+// generate timestamp
+this.salt = uuidv1();
+
+// encrypt password
+this.password_hash = this.encryptPassword(password)
+
+})
+.get(()=> this._password);
+
+
+userSchema.methods = {
+    encryptPassword: (password)=>{
+        if(!password) return ""
+        try{
+         
+            return crypto.createHmac('sha256', this.salt)
+                               .update(password)
+                               .digest('hex');
+           
+        }catch(err){
+            return "";
+        }
+    }
+}
 
 module.exports = mongoose.model('users', userSchema);
