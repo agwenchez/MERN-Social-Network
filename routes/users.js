ae458722530a776fb9bb51e6bf4abdbe40127663
agwenchez <agweraenock76@gmail.com>
2020-09-11 05:11:17 -0400

    some bug fixes

--- routes/users.js
+++ routes/users.js
@@ -1,89 +1,79 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
-const JWTsecret = 'aduifuiafbcoani'
-const User = require('../models/Users');
-const { userSignupValidator } = require('../validation/users')
+const jwt = require("jsonwebtoken");
+const JWTsecret = "aduifuiafbcoani";
+const User = require("../models/Users");
+const { userSignupValidator } = require("../validation/users");
 
 // get all users
-router.get('/all',(req,res)=>{
-    User.find()
-    .then( users =>{
-        res.status(200).json(users)
-    }).catch(err => res.status(500).json({error: err}))
-})
-
-
-
+router.get("/all", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 // user signup
-router.post('/signup', userSignupValidator,(req,res)=>{
+router.post("/signup", userSignupValidator, (req, res) => {
+  const { name, email, password } = req.body;
 
-    const {
-        name,
-        email,
-        password
-      
-    } = req.body;
+  User.findOne({
+    email,
+  })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          msg: "Email already exists",
+        });
+      } else {
+        // create new artist
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        newUser
+          .save()
+          .then((user) => {
+            res.status(200).json(user);
+            console.log(user);
+          })
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
-    User.findOne({
-        email
-    }).then(user => {
-        if (user) {
-            return res.status(400).json({
-                msg: "Email already exists"
-            })
-        } else {
-            // create new artist
-            const newUser = new User({
-                name,
-                email,
-                password
-            })
-            newUser.save()
-                .then(
-                    user => {
-                        res.status(200).json(user);
-                        console.log(user);
-                    })
-                .catch(err => console.log(err));
+router.post("/signin", (req, res) => {
+  const { _id, email, password } = req.body;
 
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) return "Please sign up before you log in";
 
-        }
+      // authenticate user, make sure password and useramematch
+      if (!user.authenticate(password)) {
+        return res.status(401).send("Email and password don't match");
+      }
 
-    }).catch(err => console.log(err));
-   
+      //if authenticated user, generate token
+      const token = jwt.sign({ _id: user._id }, JWTsecret);
 
+      //persist the token as t with expiry date
+      res.cookie("t", token, { expire: new Date() + 3600 });
 
+      // return user together with token to the client
+      const { _id, name, email } = user;
+      return res.json({ token, user: { _id, name, email } });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post('/signout', (req,res)=>{
+    res.clearCookie("t");
+    return res.json({msg: 'Log out successful'})
 })
 
-router.post('/signin', (req,res)=>{
 
-const {_id, email, password} =req.body;
-
-User.findOne({email}).then( user =>{
-
-    if(!user) return "Please sign up before you log in"
-
-    // authenticate user, make sure password and useramematch
-    if(!user.authenticate(password)){
-        return res.status(401).send('Email and password dont match')
-    }
-
-    //if authenticated user, generate token
-    const token = jwt.sign({_id:user._id}, JWTsecret)
-
-    //persist the token as t with expiry date 
-    res.cookie("t",token, {expire: new Date() + 3600})
-    
-    // return user together with token to the client
-    const {_id, name, email} =user;
-    return res.json({token,  user:{_id, name, email}})
-
-
-
-}).catch(err => console.log(err));
-
-})
-
-module.exports = router
+module.exports = router;
