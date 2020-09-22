0466a91b713c544cb2a8c71dc9eceab8f4c59f35
agwenchez <agweraenock76@gmail.com>
2020-09-22 08:32:09 -0400

    backend ruote protection with JWT

--- server.js
+++ server.js
@@ -1,43 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-const expressValidator = require('express-validator');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const expressValidator = require("express-validator");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const postRoute = require('./routes/posts')
-const userRoute = require('./routes/users')
+const postRoute = require("./routes/posts");
+const userRoute = require("./routes/users");
 
 // bodyParser Middleware
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 // expressValidator middleware
 app.use(expressValidator());
 // cookieParser iddlewae
 app.use(cookieParser());
 
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("invalid token...");
+  }
+});
 
 // DB connection
 mongoose
-    .connect(process.env.MONGO_URI,{
-    useCreateIndex:true,
-    useFindAndModify:true,
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-    })
-    .then( ()=>{
-        app.listen(port, ()=>console.log(` MongoDB connected. Listening on PORT: ${port}`));
-    })
-    .catch(
-        error =>{
-            console.log(`Unable to establish a connection to the server ${error}`)
-        }
-    )
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(port, () =>
+      console.log(` MongoDB connected. Listening on PORT: ${port}`)
+    );
+  })
+  .catch((error) => {
+    console.log(`Unable to establish a connection to the server ${error}`);
+  });
 
-   
 // import routes
-app.use('/posts', postRoute);
-app.use('/users', userRoute);
-
-
-
+app.use("/posts", postRoute);
+app.use("/users", userRoute);
