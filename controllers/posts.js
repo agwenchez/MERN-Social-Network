963fad4fd39bf5778b555855442a0443cde145bf
agwenchez <agweraenock76@gmail.com>
2020-09-24 16:34:45 -0400

    some bug fixes

--- controllers/posts.js
+++ controllers/posts.js
@@ -103,22 +103,22 @@ exports.createPost = (req, res, next) => {
 //         });
 // };
 
-// exports.isPoster = (req, res, next) => {
-//     let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
-//     let adminUser = req.post && req.auth && req.auth.role === 'admin';
+exports.isPoster = (req, res, next) => {
+    let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+    let adminUser = req.post && req.auth && req.auth.role === 'admin';
 
-//     // console.log("req.post ", req.post, " req.auth ", req.auth);
-//     // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
+    // console.log("req.post ", req.post, " req.auth ", req.auth);
+    // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
 
-//     let isPoster = sameUser || adminUser;
+    let isPoster = sameUser || adminUser;
 
-//     if (!isPoster) {
-//         return res.status(403).json({
-//             error: 'User is not authorized'
-//         });
-//     }
-//     next();
-// };
+    if (!isPoster) {
+        return res.status(403).json({
+            error: 'User is not authorized'
+        });
+    }
+    next();
+};
 
 // exports.updatePost = (req, res, next) => {
 //     let post = req.post;
